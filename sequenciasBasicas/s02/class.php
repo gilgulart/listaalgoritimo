79dfea94ddcebd421cc63aff6f6cbafc9299c4d3
gilgulart <gilbertogulart70@gmail.com>
2024-04-08 21:33:42 -0300

    exercícios

---
+++ sequenciasBasicas/s02/class.php
@@ -0,0 +1,22 @@
+<?php 
+class Worker{
+    var $worker ;
+    private $wage ;
+    
+    public function __construct($worker,$wage) {
+        $this->worker = $worker ;
+        $this->wage = $wage ;
+    }
+    public function getWage(){
+        return $this -> wage ;
+    }
+
+    public function menssage(){
+     if ($this-> worker === "Maria do Carmo"){ 
+        echo "<p> Nome do Funcionário: Maria do Carmo</p><p> Salário: {$this-> wage}</p><p> O funcionário Maria do Carmo tem um salário de R$ " . number_format($this->wage, 2,',','.') . " em Abril </p>" ;
+
+     }
+    }
+    
+}
+?>
---
+++ sequenciasBasicas/s02/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Informações sobre o funcionário</h1>
+    <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
+        <input type="text" name="worker" id="worker" placeholder="O seu nome aqui...">
+        <button type="submit">Enviar</button>
+    </form>
+    <?php 
+    $worker = $_REQUEST ['worker'] ;
+    var_dump($worker);
+    require_once "class.php" ;
+    $maria = new Worker("Maria do Carmo",1850,45) ;
+    $maria-> menssage() ;
+    ?>
+</body>
+
+</html>
